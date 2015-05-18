b6377ee97eb1904c7f2acfa06ffb4d3e5d0aef8f
Nate Fox <nfox@byu.edu>
2015-05-18 11:07:32 -0600

    changed file structure to allow for multiple levels

--- badPigs/common.js
+++ badPigs/common.js
@@ -21,9 +21,8 @@ function loadEngine()
 	setKeyEvents(player);
 	var enemies = buildEnemies();
 	var platforms = buildPlatforms();
-	buildItems();
+	var stars = buildStars();
 	setBorderCollisions();
-	// TODO add timer (or time limit)
 }
 
 /*
@@ -50,85 +49,15 @@ function buildPlayer()
 }
 
 /*
- * @desc Build and return a list of enemy objects
- */
-function buildEnemies()
-{
-	var enemies = [];
-	var dir = 1;
-	var positions = [];
-	for(var i = 0; i < 10; i++)
-		positions.push({x: i*200+100, y:300});
-	positions.push({x: 1550, y:34});
-	positions.push({x: 1550, y:34});
-	for(var i = 0; i < positions.length; i++) {
-		var enemy = new foxEngine.Image("images/pig_left.png", 60, 40, positions[i].x, positions[i].y);
-		enemy.setHFlipImages("images/pig_right.png", "images/pig_left.png");
-		enemy.setType("enemy");
-		enemy.friction = 0.0;
-		enemy.vel_max_x = 100;
-		enemy.pushX(dir * 1000000);
-		if(dir > 0)
-			enemy.faceRight();
-		dir *= -1;
-		enemy.applyGravity(true);
-		enemy.setZIndex(800);
-		enemy.onHitVerticalBorder = function() {
-			this.reverseX();
-			this.hFlip();
-		};
-		enemies.push(enemy);
-	}
-	foxEngine.addCollisionEvent("player", "enemy", playerEnemyCollision);
-	return enemies;
-}
-
-/*
- * @desc Build and return a list of platform objects
- */
-function buildPlatforms()
-{
-	var platforms = [];
-	var y = 450;
-	for(var i = 0; i < 8; i++)
-	{
-		var platform = new foxEngine.Image("images/platform.png", 150, 20, i*200+100, y);
-		y -= 50;
-		platform.setType("platform");
-		platforms.push(platform);
-	}
-	foxEngine.addCollisionEvent("player", "platform", platformCollision);
-	foxEngine.addCollisionEvent("enemy", "platform", platformCollision);
-	return platforms;
-}
-
-/*
  * @desc Build and return goal object
  */
-function buildGoal()
+function buildGoal(xPos, yPos)
 {
-	var goal = new foxEngine.Image("images/flag.png", 32, 32, 1550, 34);
+	var goal = new foxEngine.Image("images/flag.png", 32, 32, xPos, yPos);
 	goal.setType("goal");
 	goal.setZIndex(900);
 	foxEngine.addCollisionEvent("player", "goal", playerGoalCollision);
 	return goal;
-}
-
-/*
- * @desc Build other items that the player can get
- */
-function buildItems()
-{
-	var star = new foxEngine.Image("images/star.png", 30, 30, 760, 240);
-	star.setType("star");
-	foxEngine.addCollisionEvent("player", "star", function(player, star) {
-		if(!hasWeapon)
-		{
-			hasWeapon = true;
-			star.pushY(-100000);
-			setTimeout(function(){star.remove()}, 500);
-		}
-	});
 }
 
 /*
@@ -211,15 +140,6 @@ function playerEnemyCollision(player, enemy)
 }
 
 /*
- * @desc Called when a player and a goal collide
- */
-function playerGoalCollision(player, goal)
-{
-	goal.remove();
-	buildEndMessage("images/you_win.png");
-}
-
-/*
  * @desc Called when certain components and platforms collide
  */
 function platformCollision(component, platform)
@@ -277,7 +197,6 @@ function removeEnemy(enemy)
 {
 	enemy.remove();
 
-	// Show flag if all enemies are gone
 	if(foxEngine.typeComponentMap['enemy'].length == 0)
-		buildGoal();
+		allEnemiesGone();
 }
---
+++ badPigs/level1.js
@@ -0,0 +1,87 @@
+/*
+ * @desc Build and return a list of enemy objects
+ */
+function buildEnemies()
+{
+	var enemies = [];
+	var dir = 1;
+	var positions = [];
+	for(var i = 0; i < 10; i++)
+		positions.push({x: i*200+100, y:300});
+	positions.push({x: 1550, y:34});
+	positions.push({x: 1550, y:34});
+	for(var i = 0; i < positions.length; i++) {
+		var enemy = new foxEngine.Image("images/pig_left.png", 60, 40, positions[i].x, positions[i].y);
+		enemy.setHFlipImages("images/pig_right.png", "images/pig_left.png");
+		enemy.setType("enemy");
+		enemy.friction = 0.0;
+		enemy.vel_max_x = 100;
+		enemy.pushX(dir * 1000000);
+		if(dir > 0)
+			enemy.faceRight();
+		dir *= -1;
+		enemy.applyGravity(true);
+		enemy.setZIndex(800);
+		enemy.onHitVerticalBorder = function() {
+			this.reverseX();
+			this.hFlip();
+		};
+		enemies.push(enemy);
+	}
+	foxEngine.addCollisionEvent("player", "enemy", playerEnemyCollision);
+	return enemies;
+}
+
+/*
+ * @desc Build and return a list of platform objects
+ */
+function buildPlatforms()
+{
+	var platforms = [];
+	var y = 450;
+	for(var i = 0; i < 8; i++)
+	{
+		var platform = new foxEngine.Image("images/platform.png", 150, 20, i*200+100, y);
+		y -= 50;
+		platform.setType("platform");
+		platforms.push(platform);
+	}
+	foxEngine.addCollisionEvent("player", "platform", platformCollision);
+	foxEngine.addCollisionEvent("enemy", "platform", platformCollision);
+	return platforms;
+}
+
+/*
+ * @desc Build stars that the player can get
+ */
+function buildStars()
+{
+	var star = new foxEngine.Image("images/star.png", 30, 30, 760, 240);
+	star.setType("star");
+	foxEngine.addCollisionEvent("player", "star", function(player, star) {
+		if(!hasWeapon)
+		{
+			hasWeapon = true;
+			star.pushY(-100000);
+			setTimeout(function(){star.remove()}, 500);
+		}
+	});
+	return [star];
+}
+
+/*
+ * @desc Called when all enemies are gone
+ */
+function allEnemiesGone()
+{
+	buildGoal(1550, 34);
+}
+
+/*
+ * @desc Called when a player and a goal collide
+ */
+function playerGoalCollision(player, goal)
+{
+	goal.remove();
+	window.location.href = "./2.html";
+}
