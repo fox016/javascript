edd459faa3c68aa8dc72d9eb6148676d75676f6d
Nate Fox <nfox@byu.edu>
2018-01-26 10:28:29 -0700

    testing wstring c++

---
+++ ipt692/test.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <iostream>
+#include <sys/uio.h>
+#include <sys/fcntl.h>
+
+int main()
+{
+	/*
+	std::wstring test = L"北선발대 방남 이틀째…평창 개폐회식장·경기장 집중 점검";
+	std::wcout << test.size() << std::endl;
+	std::wcout << L"Got here" << std::endl;
+	std::wcout << test << std::endl;
+	std::wcout << L"Got here" << std::endl;
+	for(std::wstring::iterator it=test.begin(); it!=test.end(); it++)
+		std::wcout << *it << std::endl;
+	std::wcout << L"Got here" << std::endl;
+	*/
+
+	/*
+	std::string str = "this is a string";
+	std::cout << str.size() << std::endl;
+	std::cout << str << std::endl;
+	for(std::string::iterator it=str.begin(); it != str.end(); it++)
+		std::cout << *it << std::endl;
+	*/
+
+	/*
+	std::string test = "北선발대 방남 이틀째…평창 개폐회식장·경기장 집중 점검";
+	std::cout << test.size() << std::endl;
+	std::cout << "Got here" << std::endl;
+	std::cout << test << std::endl;
+	std::cout << "Got here" << std::endl;
+	for(std::string::iterator it=test.begin(); it!=test.end(); it++)
+		std::cout << *it << std::endl;
+	std::cout << "Got here" << std::endl;
+	*/
+}
+
