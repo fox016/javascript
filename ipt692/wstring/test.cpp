ce76ec4c90e372ef4571fd449a71281fc480b082
Nate Fox <nfox@byu.edu>
2018-02-01 10:28:15 -0700

    wstring with map working

--- ipt692/wstring/test.cpp
+++ ipt692/wstring/test.cpp
@@ -1,64 +1,59 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <map>
 #include "dirent.h"
 using namespace std;
 
-void processLine(wchar_t* line, wofstream &out)
+void processLine(wchar_t* line, map<wstring, int> &words)
 {
-	locale::global(locale("")); // https://stackoverflow.com/a/3950795/1060631 (might be different for Windows)
+	// TODO
 	wstring wline(line);
-	out << wline << endl;
-	out << wline.size() << endl;
-	for(wstring::iterator it=wline.begin(); it!=wline.end(); it++)
-		out << *it << endl;
+	words[wline] = 1;
 }
 
-bool readfile(char* filename, string path)
+bool readfile(char* filename, string path, map<wstring, int> &words)
 {
 	if(strcmp(filename, ".") == 0)
 		return false;
 	if(strcmp(filename, "..") == 0)
 		return false;
-
 	string fn(filename);
 	fn = path + "/" + fn;
 
-	wchar_t line[1000];
+	// Setup input
+	int lineSize = 1000;
+	wchar_t line[lineSize]; // char array to hold lines as they are read from file
 	wifstream in;
-	wofstream out; // https://stackoverflow.com/a/18226387/1060631
 	in.open(fn, wifstream::in);
-	out.open("out.txt", wofstream::out | wofstream::binary | wofstream::trunc);
-
 	if(!in.is_open()) {
 		perror("Could not open infile");
 		return false;
 	}
-	if(!out.is_open()) {
-		perror("Could not open outfile");
-		return false;
-	}
 
-	while(in.getline(line, 1000)) {
-		processLine(line, out);
+	// Read in one line at a time
+	while(in.getline(line, lineSize)) {
+		processLine(line, words);
 	}
-
 	in.close();
-	out.close();
 
 	return true;
 }
 
 int readfiles()
 {
-	DIR *dir;
+	// Set up map structure to contain words and word counts
+	map<wstring, int> words;
+
+	// Read each file in a directory
+	DIR *dir; // https://stackoverflow.com/a/612176/1060631
 	struct dirent *ent;
-	string path = "./files";
+	string path = "./files"; // Path to directory to read files from
 	dir = opendir(path.c_str());
 	if(dir != NULL) {
-		ent = readdir(dir);
+		ent = readdir(dir); // Read entity from directory
 		while (ent != NULL) {
-			readfile(ent->d_name, path);
+			readfile(ent->d_name, path, words); // Read each file in directory
 			ent = readdir(dir);
 		}
 		closedir(dir);
@@ -66,6 +61,22 @@ int readfiles()
 		perror("Could not open directory");
 		return EXIT_FAILURE;
 	}
+
+	// Output
+	locale::global(locale("")); // https://stackoverflow.com/a/3950795/1060631 (might be different for Windows)
+	wofstream out; // https://stackoverflow.com/a/18226387/1060631
+	out.open("out.txt", wofstream::out | wofstream::binary | wofstream::trunc);
+	if(!out.is_open()) {
+		perror("Could not open outfile");
+		return false;
+	}
+	for(map<wstring,int>::iterator it = words.begin(); it != words.end(); it++)
+	{
+		wstring word = it->first;
+		out << word << endl;
+	}
+	out.close();
+
 	return 0;
 }
 
