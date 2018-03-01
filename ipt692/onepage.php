1e18900a88015bcfa6ab2109436c5471beba93fe
Nathan Fox <fox016@gmail.com>
2018-03-01 13:06:20 -0700

    99 bottles of milk

--- ipt692/onepage.php
+++ ipt692/onepage.php
@@ -1,3 +1,40 @@
+<?php
+
+$output = "";
+
+if(isset($_POST['number'])) // If they hit go
+{
+  $num = $_POST['number']; // Create $num variable
+  $num = intval($num); // intval is PHP's parseFloat (takes a string number and returns it as a number type)
+  if(is_int($num) && $num !== "") // If this is a number
+  {
+    for($startNum = $num; $startNum >= 1; $startNum = $startNum-1) // Same as JavaScript
+    {
+      $verse = getVerse($startNum);
+      $output = $output . $verse; // . is how to + 2 strings together in PHP
+    }
+  }
+  else // If this is not a number
+  {
+    $output = "This is not a number";
+  }
+}
+else // Initial load
+{
+}
+
+function getVerse($number) // Functions same as JavaScript
+{
+  $nextNum = $number-1;
+  if($number == 1) // If-else-if blocks are same as JavaScript
+    return "<div class='verse'>$number bottle of milk on the wall, $number bottles of milk. Take one down, pass it around, $nextNum bottles of milk on the wall.</div>";
+  else if($nextNum == 1)
+    return "<div class='verse'>$number bottles of milk on the wall, $number bottle of milk. Take one down, pass it around, $nextNum bottle of milk on the wall.</div>";
+  else
+    return "<div class='verse'>$number bottles of milk on the wall, $number bottles of milk. Take one down, pass it around, $nextNum bottles of milk on the wall.</div>";
+}
+
+?>
 <!DOCTYPE>
 <html>
 <style>
@@ -7,27 +44,18 @@ body,input,button {
 input[type=submit] {
   font-size:1.5em;
 }
+.verse {
+  display:block;
+  margin:10px 0;
+}
 </style>
 <body>
   <form method='POST'>
-    <input type='text' name='name' placeholder='Enter your name' />
+    <input type='number' name='number' placeholder='Enter a number' />
     <button type='submit'>Go</button>
   </form>
-  <div>
-  <?php
-    if(isset($_POST['name']) && $_POST['name'] != "")
-    {
-      echo "Your name is " . $_POST['name'];
-    }
-    else if(!isset($_POST['name']))
-    {
-      echo "Enter your name";
-    }
-    else
-    {
-      echo "You didn't enter your name, silly";
-    }
-  ?>
+  <div id='lyrics-holder'>
+  <?php echo $output; ?>
   </div>
 </body>
 </html>
